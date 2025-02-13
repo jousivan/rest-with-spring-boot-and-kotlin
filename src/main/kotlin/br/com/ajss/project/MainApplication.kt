c61c80c2c8e32bb32e13b9de503b5c572a6a9cae
jousivan <ajssti@gmail.com>
2025-02-13 09:49:09 -0300

    Implementation MathCalculator

--- src/main/kotlin/br/com/ajss/project/MainApplication.kt
+++ src/main/kotlin/br/com/ajss/project/MainApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class RestWithSpringBootAndKotlinApplication
+class MainApplication
 
 fun main(args: Array<String>) {
-	runApplication<RestWithSpringBootAndKotlinApplication>(*args)
+	runApplication<MainApplication>(*args)
 }
