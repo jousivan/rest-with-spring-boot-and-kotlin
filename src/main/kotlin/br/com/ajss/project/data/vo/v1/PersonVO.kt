b06fb0306382dd4df57d0dbfc2a458ba5481ba4e
jousivan <ajssti@gmail.com>
2025-02-23 23:15:17 -0300

    Implementation of VOs and Flyway migrations

--- src/main/kotlin/br/com/ajss/project/data/vo/v1/PersonVO.kt
+++ src/main/kotlin/br/com/ajss/project/data/vo/v1/PersonVO.kt
@@ -1,10 +1,16 @@
 package br.com.ajss.project.data.vo.v1
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+
+@JsonPropertyOrder("id", "address", "first_name", "last_name", "gender")
 data class PersonVO(
 
     var id: Long = 0,
     var firstName: String = "",
     var lastName: String = "",
     var address: String = "",
+
+    @field:JsonIgnore
     var gender: String = ""
 )
