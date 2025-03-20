fde7cf91c56b9bfccd8380d8f78ae0458cb81789
jousivan <ajssti@gmail.com>
2025-03-19 21:50:41 -0300

    Added Swagger Configurations

--- src/main/kotlin/br/com/ajss/project/config/SwaggerConfig.kt
+++ src/main/kotlin/br/com/ajss/project/config/SwaggerConfig.kt
@@ -1,35 +1,26 @@
 package br.com.ajss.project.config
 
-import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
-import io.swagger.v3.oas.models.security.SecurityRequirement
-import io.swagger.v3.oas.models.security.SecurityScheme
+import io.swagger.v3.oas.models.info.License
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class SwaggerConfig {
+class OpenApiConfig {
 
     @Bean
-    fun customOpenAPI(): OpenAPI {
+    fun customOpenApi(): OpenAPI {
         return OpenAPI()
             .info(
                 Info()
-                    .title("API de Exemplo")
-                    .version("1.0")
-                    .description("Documentação da API de Exemplo com OpenAPI")
-            )
-            .addSecurityItem(SecurityRequirement().addList("BearerAuth"))
-            .components(
-                Components()
-                    .addSecuritySchemes(
-                        "BearerAuth",
-                        SecurityScheme()
-                            .name("BearerAuth")
-                            .type(SecurityScheme.Type.HTTP)
-                            .scheme("bearer")
-                            .bearerFormat("JWT")
+                    .title("RESTful API with Kotlin 1.6.10 and Spring Boot 3.0.0")
+                    .version("v1")
+                    .description("Some description about your API.")
+                    .termsOfService("https://pub.erudio.com.br/meus-cursos")
+                    .license(
+                        License().name("Apache 2.0")
+                            .url("https://pub.erudio.com.br/meus-cursos")
                     )
             )
     }
--- src/main/kotlin/br/com/ajss/project/controller/PersonController.kt
+++ src/main/kotlin/br/com/ajss/project/controller/PersonController.kt
@@ -3,13 +3,15 @@ package br.com.ajss.project.controller
 import br.com.ajss.project.data.vo.v1.PersonVO
 import br.com.ajss.project.services.PersonService
 import br.com.ajss.project.util.MediaType
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import br.com.ajss.project.data.vo.v2.PersonVO as PersonVO2
 
 @RestController
 @RequestMapping("/api/person/v1")
+@Tag(name = "People", description = "Endpoints for Managing People")
 class PersonController {
 
 
@@ -18,18 +20,239 @@ class PersonController {
     // var service: PersonService = PersonService()
 
     @GetMapping(produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML])
+    @Operation(summary = "Find all people", description = "Find all people recorded in the database",
+        tags = ["People"],
+        responses = [
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "List of people returned",
+                responseCode = "200",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        array = io.swagger.v3.oas.annotations.media.ArraySchema(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = PersonVO::class
+                            )
+                        )
+                    )
+                ]
+            ),
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "No Content",
+                responseCode = "204",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        array = io.swagger.v3.oas.annotations.media.ArraySchema(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = Unit::class
+                            )
+                        )
+                    )
+                ]
+            ),
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "Bad Request",
+                responseCode = "400",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        array = io.swagger.v3.oas.annotations.media.ArraySchema(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = Unit::class
+                            )
+                        )
+                    )
+                ]
+            ),
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "Unauthorized",
+                responseCode = "401",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        array = io.swagger.v3.oas.annotations.media.ArraySchema(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = Unit::class
+                            )
+                        )
+                    )
+                ]
+            ),
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "Not Found",
+                responseCode = "404",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        array = io.swagger.v3.oas.annotations.media.ArraySchema(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = PersonVO::class
+                            )
+                        )
+                    )
+                ]
+            ),
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "Internal Server Error",
+                responseCode = "500",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        array = io.swagger.v3.oas.annotations.media.ArraySchema(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = PersonVO::class
+                            )
+                        )
+                    )
+                ]
+            )
+        ]
+    )
+
+
     fun findAll(): List<PersonVO> {
         return service.findAll()
     }
 
     @GetMapping(value = ["/{id}"],
         produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML])
+    @Operation(summary = "Finds a person", description = "Finds a person by ID",
+        tags = ["People"],
+        responses = [
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "List of people returned",
+                responseCode = "200",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = PersonVO::class
+                            )
+                    )
+                ]
+            ),
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "No Content",
+                responseCode = "204",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        array = io.swagger.v3.oas.annotations.media.ArraySchema(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = Unit::class
+                            )
+                        )
+                    )
+                ]
+            ),
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "Bad Request",
+                responseCode = "400",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        array = io.swagger.v3.oas.annotations.media.ArraySchema(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = Unit::class
+                            )
+                        )
+                    )
+                ]
+            ),
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "Unauthorized",
+                responseCode = "401",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        array = io.swagger.v3.oas.annotations.media.ArraySchema(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = Unit::class
+                            )
+                        )
+                    )
+                ]
+            ),
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "Not Found",
+                responseCode = "404",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        array = io.swagger.v3.oas.annotations.media.ArraySchema(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = PersonVO::class
+                            )
+                        )
+                    )
+                ]
+            ),
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "Internal Server Error",
+                responseCode = "500",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        array = io.swagger.v3.oas.annotations.media.ArraySchema(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = PersonVO::class
+                            )
+                        )
+                    )
+                ]
+            )
+        ]
+    )
     fun findById(@PathVariable(value="id") id: Long): PersonVO {
         return service.findById(id)
     }
 
     @PostMapping(consumes = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML],
         produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML])
+    @Operation(summary = "Create a new person", description = "Create a new person",
+        tags = ["People"],
+        responses = [
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "List of people returned",
+                responseCode = "200",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        schema = io.swagger.v3.oas.annotations.media.Schema(
+                            implementation = PersonVO::class
+                        )
+                    )
+                ]
+            ),
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "Bad Request",
+                responseCode = "400",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        array = io.swagger.v3.oas.annotations.media.ArraySchema(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = Unit::class
+                            )
+                        )
+                    )
+                ]
+            ),
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "Unauthorized",
+                responseCode = "401",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        array = io.swagger.v3.oas.annotations.media.ArraySchema(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = Unit::class
+                            )
+                        )
+                    )
+                ]
+            ),
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "Internal Server Error",
+                responseCode = "500",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        array = io.swagger.v3.oas.annotations.media.ArraySchema(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = PersonVO::class
+                            )
+                        )
+                    )
+                ]
+            )
+        ]
+    )
     fun create(@RequestBody person: PersonVO): PersonVO {
         return service.create(person)
 
@@ -46,12 +269,152 @@ class PersonController {
 
     @PutMapping(consumes = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML],
         produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML])
+    @Operation(summary = "Update a person", description = "Update a person",
+        tags = ["People"],
+        responses = [
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "No Content",
+                responseCode = "204",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        array = io.swagger.v3.oas.annotations.media.ArraySchema(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = Unit::class
+                            )
+                        )
+                    )
+                ]
+            ),
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "Bad Request",
+                responseCode = "400",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        array = io.swagger.v3.oas.annotations.media.ArraySchema(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = Unit::class
+                            )
+                        )
+                    )
+                ]
+            ),
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "Unauthorized",
+                responseCode = "401",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        array = io.swagger.v3.oas.annotations.media.ArraySchema(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = Unit::class
+                            )
+                        )
+                    )
+                ]
+            ),
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "Not Found",
+                responseCode = "404",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        array = io.swagger.v3.oas.annotations.media.ArraySchema(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = PersonVO::class
+                            )
+                        )
+                    )
+                ]
+            ),
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "Internal Server Error",
+                responseCode = "500",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        array = io.swagger.v3.oas.annotations.media.ArraySchema(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = PersonVO::class
+                            )
+                        )
+                    )
+                ]
+            )
+        ]
+    )
     fun update(@RequestBody person: PersonVO): PersonVO {
         return service.update(person)
     }
 
     @DeleteMapping(value = ["/{id}"],
         produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML])
+    @Operation(summary = "Delete a person", description = "Delete a person",
+        tags = ["People"],
+        responses = [
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "No Content",
+                responseCode = "204",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        array = io.swagger.v3.oas.annotations.media.ArraySchema(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = Unit::class
+                            )
+                        )
+                    )
+                ]
+            ),
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "Bad Request",
+                responseCode = "400",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        array = io.swagger.v3.oas.annotations.media.ArraySchema(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = Unit::class
+                            )
+                        )
+                    )
+                ]
+            ),
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "Unauthorized",
+                responseCode = "401",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        array = io.swagger.v3.oas.annotations.media.ArraySchema(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = Unit::class
+                            )
+                        )
+                    )
+                ]
+            ),
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "Not Found",
+                responseCode = "404",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        array = io.swagger.v3.oas.annotations.media.ArraySchema(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = PersonVO::class
+                            )
+                        )
+                    )
+                ]
+            ),
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                description = "Internal Server Error",
+                responseCode = "500",
+                content = [
+                    io.swagger.v3.oas.annotations.media.Content(
+                        array = io.swagger.v3.oas.annotations.media.ArraySchema(
+                            schema = io.swagger.v3.oas.annotations.media.Schema(
+                                implementation = PersonVO::class
+                            )
+                        )
+                    )
+                ]
+            )
+        ]
+    )
     fun delete(@PathVariable(value="id") id: Long) : ResponseEntity<*>{
         service.delete(id)
         return ResponseEntity.noContent().build<Any>()
